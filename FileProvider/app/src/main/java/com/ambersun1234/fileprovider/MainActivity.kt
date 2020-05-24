b4cb4b595cf24650b49c8614db8de8da565fa690
ambersun <ambersun1019.shawn@gmail.com>
2020-05-25 01:37:41 +0800

    Add basic Implicit Intent with uri

---
+++ FileProvider/app/src/main/java/com/ambersun1234/fileprovider/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.ambersun1234.fileprovider
+
+import android.app.SearchManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btn_arr: Array<Int>
+
+    val onClickHandler = View.OnClickListener { v ->
+        val it = Intent(Intent.ACTION_VIEW)
+        val myid = v?.id
+
+        when (myid) {
+            R.id.btn_email -> {
+                it.action = Intent.ACTION_SENDTO
+                it.data = Uri.parse("mailto:ambersun1019.shawn@gmail.com")
+                it.putExtra(Intent.EXTRA_CC, "xxx@gmail.com")
+                it.putExtra(Intent.EXTRA_SUBJECT, "uri intent test")
+                it.putExtra(Intent.EXTRA_TEXT, "testing")
+            }
+
+            R.id.btn_web -> {
+                it.action = Intent.ACTION_VIEW
+                it.data = Uri.parse("https://facebook.com")
+            }
+
+            R.id.btn_websearch -> {
+                it.action = Intent.ACTION_WEB_SEARCH
+                it.putExtra(SearchManager.QUERY, "mysearch")
+            }
+
+            R.id.btn_gps -> {
+                it.action = Intent.ACTION_VIEW
+                it.data = Uri.parse("geo:25.047095,121.517308")
+            }
+
+            R.id.btn_text -> {
+                it.action = Intent.ACTION_VIEW
+                it.data = Uri.parse("sms:0912-345678?body=您好！")
+            }
+
+            R.id.btn_tel -> {
+                // check permission
+                val chk_permission = ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.CALL_PHONE)
+                var lala = 1
+
+                if (chk_permission != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, android.Manifest.permission.READ_CONTACTS)) {
+                        Unit
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(android.Manifest.permission.CALL_PHONE),
+                            lala
+                        )
+                    }
+                }
+                else {
+                    it.action = Intent.ACTION_CALL
+                    it.data = Uri.parse("tel:0958-967228")
+                }
+            }
+        }
+
+        startActivity(it)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.btn_arr = arrayOf(
+            R.id.btn_email, R.id.btn_tel,
+            R.id.btn_gps, R.id.btn_text,
+            R.id.btn_web, R.id.btn_websearch
+        )
+        for (i: Int in this.btn_arr) {
+            val tmp = findViewById<Button>(i)
+            tmp.setOnClickListener(this.onClickHandler)
+        }
+    }
+}
