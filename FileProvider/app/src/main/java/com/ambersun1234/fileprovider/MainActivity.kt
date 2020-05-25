dfcc2e351343257279896959a2fb28e7fb9b0120
ambersun <ambersun1019.shawn@gmail.com>
2020-05-25 11:37:49 +0800

    Add broadcast sender/receiver

--- FileProvider/app/src/main/java/com/ambersun1234/fileprovider/MainActivity.kt
+++ FileProvider/app/src/main/java/com/ambersun1234/fileprovider/MainActivity.kt
@@ -1,21 +1,55 @@
 package com.ambersun1234.fileprovider
 
 import android.app.SearchManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    private companion object {
+        const val MY_MSG = "com.ambersun1234.fileprovider.broadcast"
+    }
     private lateinit var btn_arr: Array<Int>
+    private val broadcastHandler = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            var toastMsg = ""
 
-    val onClickHandler = View.OnClickListener { v ->
+            when (intent?.action) {
+                Intent.ACTION_TIME_CHANGED -> {
+                    toastMsg = "Receive! your system time has changed"
+                }
+
+                Intent.ACTION_BATTERY_CHANGED -> {
+                    val level = intent.getIntExtra("level", 0)
+                    val scale = intent.getIntExtra("scale", 100)
+                    toastMsg = "Receive! your battery " + (level * 100 / scale) + "%"
+                }
+
+                MainActivity.MY_MSG -> {
+                    toastMsg = "Receive! we received $MY_MSG"
+                }
+            }
+
+            Toast.makeText(
+                this@MainActivity,
+                toastMsg,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private val onClickHandler = View.OnClickListener { v ->
         val it = Intent(Intent.ACTION_VIEW)
         val myid = v?.id
 
@@ -51,7 +85,7 @@ class MainActivity : AppCompatActivity() {
             R.id.btn_tel -> {
                 // check permission
                 val chk_permission = ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.CALL_PHONE)
-                var lala = 1
+                val lala = 1
 
                 if (chk_permission != PackageManager.PERMISSION_GRANTED) {
                     if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, android.Manifest.permission.READ_CONTACTS)) {
@@ -88,5 +122,19 @@ class MainActivity : AppCompatActivity() {
             val tmp = findViewById<Button>(i)
             tmp.setOnClickListener(this.onClickHandler)
         }
+
+        // custom send broadcast
+        findViewById<Button>(R.id.btn_sendbroadcast).setOnClickListener{
+            val it = Intent()
+            it.action = MainActivity.MY_MSG
+            sendBroadcast(it)
+        }
+
+        // register broadcast
+        val itf = IntentFilter()
+        itf.addAction(Intent.ACTION_TIME_CHANGED)
+        itf.addAction(Intent.ACTION_BATTERY_CHANGED)
+        itf.addAction(MainActivity.MY_MSG)
+        registerReceiver(this.broadcastHandler, itf)
     }
 }
